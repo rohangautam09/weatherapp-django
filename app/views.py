ad9037c538e8ac097459780bf5b30bb50436c153
Rohan <rohangautam9@yahoo.com>
2020-04-13 21:46:55 +0530

    ui changes

--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ import requests
 def home(request):
 	if request.method == 'POST':
 		url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=cc355cd2dc64c8cf41bad66b64e790f2'
-		city = request.POST.get('place')
+		city = 'Delhi'
+		#city = request.POST.get('place')
 		r = requests.get(url.format(city)).json()
 
 		city_weather = {
@@ -14,6 +15,8 @@ def home(request):
 			'icon' : r['weather'][0]['icon']
 
 		}
+
+		
 
 		context = {'city_weather': city_weather}
 		return render(request,'app/home.html',context)
